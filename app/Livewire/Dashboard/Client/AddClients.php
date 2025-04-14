208e4b5ca3249e3eb85378ab96068aca01681b69
piouskenny <kehindeu13@gmail.com>
2025-04-14 08:21:08 +0100

    cleaning up for push

---
+++ app/Livewire/Dashboard/Client/AddClients.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Client;
+
+use Livewire\Component;
+
+class AddClients extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.client.add-clients');
+    }
+}
